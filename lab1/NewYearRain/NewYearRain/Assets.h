8fc844b600f28a5bca21c98e961186ae2650c182
alekseykondus <60347730+alekseykondus@users.noreply.github.com>
2021-03-01 22:41:08 +0200

    Added a class for working with game resources (textures, fonts, sounds)

---
+++ lab1/NewYearRain/NewYearRain/Assets.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Assets {
+public:
+	// Constructor
+	Assets() { };
+
+	// Accessors
+	[[nodiscard]] inline sf::Font& GetFontLemon() { return m_FontLemon; }
+	[[nodiscard]] inline sf::Font& GetFontAllura() { return m_FontAllura; }
+	[[nodiscard]] inline sf::Texture& GetBackgroundTexture() { return m_BackgroundTexture; }
+	[[nodiscard]] inline sf::Texture& GetÑapTexture() { return m_ÑapTexture; }
+	[[nodiscard]] inline sf::Texture& GetEndGameTexture() { return m_EndGameTexture; }
+	[[nodiscard]] inline std::vector <sf::Texture>& GetGiftsTexture() { return m_GiftsTexture; }
+	[[nodiscard]] inline sf::SoundBuffer& GetSoundBufferFail() { return m_SoundBufferFail; }
+	[[nodiscard]] inline sf::SoundBuffer& GetSoundBufferEndGame() { return m_SoundBufferEndGame; }
+
+	// Modifiers
+
+	// Public methods
+	static Assets& Instance() {
+		static Assets r;
+		return r;
+	}
+	void Load();
+
+private:
+	// Member data
+	sf::Font		 m_FontLemon;
+	sf::Font		 m_FontAllura;
+	sf::Texture      m_BackgroundTexture;
+	sf::Texture      m_ÑapTexture;
+	sf::Texture      m_EndGameTexture;
+	std::vector <sf::Texture> m_GiftsTexture;
+	sf::SoundBuffer  m_SoundBufferFail;
+	sf::SoundBuffer  m_SoundBufferEndGame;
+}; // class Assets
+
+void Assets::Load() {
+	m_GiftsTexture.resize(6);
+
+	m_FontAllura.loadFromFile("fonts\\Allura.ttf");
+	m_FontLemon.loadFromFile("fonts\\Lemon.ttf");
+
+	m_GiftsTexture[0].loadFromFile("images\\p0.png");
+	m_GiftsTexture[1].loadFromFile("images\\p1.png");
+	m_GiftsTexture[2].loadFromFile("images\\p2.png");
+	m_GiftsTexture[3].loadFromFile("images\\p3.png");
+	m_GiftsTexture[4].loadFromFile("images\\p4.png");
+	m_GiftsTexture[5].loadFromFile("images\\p5.png");
+
+	m_SoundBufferFail.loadFromFile("music\\Fail.ogg");
+	m_SoundBufferEndGame.loadFromFile("music\\EndGame.ogg");
+
+	m_BackgroundTexture.loadFromFile("images\\Background.png");
+	m_ÑapTexture.loadFromFile("images\\Cap.png");
+	//    m_GiftsTexture.loadFromFile("images\\Gifts.png");
+	m_EndGameTexture.loadFromFile("images\\EndGame.png");
+}
